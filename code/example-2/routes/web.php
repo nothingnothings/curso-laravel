07236f2560543c31e734a2b71f89e48a41d2bc6c
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2024-09-24 17:11:39 -0300

    finished lesson 2

---
+++ code/example-2/routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('/about', function () {
+//     return view('about');
+// });
+
+// Route::get('/about', function () {
+//     return 'The About Page';
+// });
+
+// * Useful for APIs
+Route::get('/about', function () {
+    return ['foo' => 'bar'];
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
