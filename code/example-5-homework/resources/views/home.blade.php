048824b66a30abe27e1c7585765f8b3c5c31c0d3
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2024-09-25 14:45:43 -0300

    Finished homework

---
+++ code/example-5-homework/resources/views/home.blade.php
@@ -0,0 +1,7 @@
+<!-- THIS IS HOW WE REFERENCE COMPONENTS, IN LARAVEL: -->
+<x-layout>
+    <x-slot:heading>
+        Home Page
+    </x-slot:heading>
+    <h1>Hello from the HOME page</h1>
+</x-layout>
