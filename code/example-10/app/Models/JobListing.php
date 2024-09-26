57982cdd4618bdb826033c2dc38ebc50fe28326d
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2024-09-26 14:53:08 -0300

    finished lesson 10

--- code/example-10/app/Models/JobListing.php
+++ code/example-10/app/Models/JobListing.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
 
 class JobListing extends Model
 {
     // protected $table = 'job_listings'; // * do this if you want to use a different table name (ex: model name is "Job", but table name is "job_listings")
 
+    use HasFactory;
 
     // * This represents 'ALL THE ATTRIBUTES THAT ARE ALLOWED TO BE MASS-ASSIGNED' (and only these attributes; attributes that are not specified here cannot be mass-assigned).
     protected $fillable = [
