54c249325d8d31561d7fe2be35c769d1a3933d82
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2024-09-26 17:52:29 -0300

    finished lesson 12 and homework

---
+++ code/example-12/app/Models/Tag.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Tag extends Model
+{
+    use HasFactory;
+
+    public function jobs(): BelongsToMany
+    {
+        return $this->belongsToMany(JobListing::class, relatedPivotKey: 'job_listing_id'); // * second parameter is used to be more explicit about the pivot table related key column's name.
+    }
+}
