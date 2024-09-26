36a0c500a43a94fbfdeca92d848e2a87f24c5568
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2024-09-26 19:12:08 -0300

    Finished lesson 13

---
+++ code/example-13/resources/views/about.blade.php
@@ -0,0 +1,7 @@
+<!-- THIS IS HOW WE REFERENCE COMPONENTS, IN LARAVEL: -->
+<x-layout>
+    <x-slot:heading>
+        About Page
+    </x-slot:heading>
+    <h1>Hello from the ABOUT page.</h1>
+</x-layout>
