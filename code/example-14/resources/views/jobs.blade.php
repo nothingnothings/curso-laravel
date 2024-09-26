a2f6b7ae87b82510603809e1db681ac60df07a4f
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2024-09-26 19:35:41 -0300

    studying lesson 14

---
+++ code/example-14/resources/views/jobs.blade.php
@@ -0,0 +1,21 @@
+<x-layout>
+    <x-slot:heading>
+        Jobs Page
+    </x-slot:heading>
+    <ul class="space-y-4">
+        @foreach($jobs as $job)
+        <a class="text-gray-500 hover:underline hover:text-blue-500 block px-4 py-6 border border-gray-200 rounded-lg" href="/jobs/{{ $job['id'] }}">
+
+            <li>
+            <div class="text-blue-500 font-bold">{{ $job->employer->name }}</div>
+            <strong>{{ $job['title'] }}: Pays {{ $job['salary'] }} per year.</strong>
+            </li>
+        </a>
+        @endforeach
+    </ul>
+
+    <div>
+        {{  $jobs->links() }}
+    </div>
+
+</x-layout>
---
+++ code/example-14/routes/web.php
@@ -0,0 +1,57 @@
+<?php
+
+use App\Models\Employer;
+use App\Models\JobListing;
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/', function () {
+
+    $jobs = JobListing::all();
+
+
+    // dd($jobs[0]); // we can access rows in the collection, as if it was an array.
+
+    // dd($jobs[0]->salary) // We can also access column values, inside of one of those row, as if it was an object.
+    // dd($jobs[0]->title) // We can also access column values, inside of one of those row, as if it was an object.
+
+
+    return view('home', [
+        'greeting' => 'Hello',
+        'name' => 'Arthur',
+        'age' => 26,
+        'job' => 'programmer',
+        'jobs' => $jobs
+    ]);
+});
+
+
+Route::get('/jobs', function () {
+
+    // $jobsWithEmployers = JobListing::with('employer')->get(); // ! No pagination:
+
+    $jobsWithEmployers = JobListing::with('employer')->paginate(3); // * Pagination applied.
+
+    return view('jobs', [
+        'jobs' => $jobsWithEmployers
+    ]);
+});
+
+
+Route::get('/jobs/{id}', function ($id) {
+    $selectedJob = JobListing::find($id);
+
+    return view('job', [
+        'job' => $selectedJob,
+    ]);
+});
+
+Route::get('/about', function () {
+
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
