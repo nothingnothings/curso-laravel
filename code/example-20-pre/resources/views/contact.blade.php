9f60f8d24e4f4a2b5e5e4c67db3afc979dca2a23
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2024-09-28 17:10:19 -0300

    studying lesson 20

---
+++ code/example-20-pre/resources/views/contact.blade.php
@@ -0,0 +1,7 @@
+<!-- THIS IS HOW WE REFERENCE COMPONENTS, IN LARAVEL: -->
+<x-layout>
+    <x-slot:heading>
+        Contact Page
+    </x-slot:heading>
+    <h1>Hello from the CONTACT page</h1>
+</x-layout>
