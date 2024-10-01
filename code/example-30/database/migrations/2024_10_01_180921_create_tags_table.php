8eaeb0a8c2ece2ea016ca33ac45299c830ee0c38
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2024-10-01 16:49:29 -0300

    prepared final lesson's code

---
+++ code/example-30/database/migrations/2024_10_01_180921_create_tags_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\Job;
+use App\Models\Tag;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->timestamps();
+        });
+
+        // Create Pivot table between tags and jobs:
+        Schema::create('job_tag', function (Blueprint $table) {
+            $table->foreignIdFor(Job::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Tag::class)->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tags');
+    }
+};
