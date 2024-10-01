00da20ea1d7dbebb38c8d0e397bacbd8e187240d
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2024-10-01 16:38:40 -0300

    finished lesson 29

---
+++ code/example-29/database/seeders/EmployerSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Employer;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class EmployerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        $employers = Employer::factory(30)->create();
+    }
+}
---
+++ code/example-29/resources/views/components/panel.blade.php
@@ -0,0 +1,3 @@
+<div {{ $attributes->merge(['class' => 'p-4 bg-white/5 rounded-xl border border-transparent group hover:border-blue-600 transition-colors duration-500'])}}>
+    {{ $slot }}
+</div>
