ac252d2fcc2a7cd0243017999bd6bb221f3e3eb0
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2024-10-01 02:51:11 -0300

    finished lesson 28

---
+++ code/example-28/resources/views/components/job-card.blade.php
@@ -0,0 +1,18 @@
+                <x-panel class="flex flex-col text-center">
+                    <div class="self-start text-sm">Laracasts</div>
+
+                    <div class="py-8">
+                        <h3 class="group-hover:text-blue-600 text-lg font-bold transition-colors duration-500">Video Producer</h3>
+                        <p class="text-sm mt-4">Full Time - from $100,000</p>
+                    </div>
+
+                    <div class="flex justify-between items-center mt-auto">
+                        <div>
+                            <x-tag size="small">Tag1</x-tag>
+                            <x-tag size="small">Tag2</x-tag>
+                            <x-tag size="small">Tag3</x-tag>
+                        </div>
+
+                        <x-employer-logo :width="42" />
+                    </div>
+                </x-panel>
