3ad9c644701dffbecdff1d7a267c310ade5ec387
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2024-10-01 19:31:51 -0300

    studying final lesson

--- code/example-30/resources/views/components/layout.blade.php
+++ code/example-30/resources/views/components/layout.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,7 +10,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@400;500;600&display=swap" rel="stylesheet">
     @vite(['resources/js/app.js'])
 </head>
-<body class="bg-black text-white font-hanken-grotesk">
+
+<body class="bg-black text-white font-hanken-grotesk pb-20">
     <div class="px-10">
         <nav class="flex justify-between items-center py-4 border-b border-white/10">
             <div>
@@ -23,7 +25,19 @@
                 <a href="#">Salaries</a>
                 <a href="#">Companies</a>
             </div>
-            <a href="#">Post a Job</a>
+
+            @auth
+                <div>
+                    <a href="/jobs/create">Post a Job</a>
+                </div>
+            @endauth
+
+            @guest
+                <div class="space-x-6 font-bold">
+                    <a href="/register">Sign Up</a>
+                    <a href="/login">Log in</a>
+                </div>
+            @endguest
         </nav>
 
         <main class="mt-10 max-w-[986px] mx-auto">
@@ -31,4 +45,5 @@
         </main>
     </div>
 </body>
+
 </html>
